6259aca218f051872af12f4d8b7dc95735d9791d
naylor-kt <116875300+naylor-kt@users.noreply.github.com>
2022-12-22 12:57:30 +0000

    Update Freesurfer.sh

--- Freesurfer.sh
+++ Freesurfer.sh
@@ -1,20 +1,27 @@
-#Define the initial function
+#!/bin/bash
+# This script contains the preparation of the T1 image ready for freesurfer, the reconstruction of the cortical surface, and the conversion of file types.
+
+# Define the initial function (preparation of the T1 image, and the execution of freesurfer) 
+
 free_surfing (){
+
+# Make a new directory for the freesurfer files
 mkdir -p $HOME/Brain_States/Freesurfer
 
+#Set the regular data_path, as has been used in the preprocessing script
 data_path="$HOME/Brain_States"
 
+#Set a new freesurfer path (just to simplfy the later code)
 fs_path="$HOME/Brain_States/Freesurfer";s=$1
 
 #PREPARE THE T1 IMAGE
-
 # Make a directory for the free surfer files
 mkdir -p ${fs_path}/Struct/$s
 
-#Copy of the T1 image which had previously undergone robust fov
+#Copy of the T1 image which had previously undergone robust fov (for registration in the preprocessing code)
 cp ${data_path}/Registration/${s}/Struct/${s}_crop_struct.nii.gz ${fs_path}/Struct/$s/${s}_T1.nii.gz
 
-#Bias Correction
+#Bias Correction (nonpve prevents fast from performing segmentation of the image) 
 fast -B --nopve ${fs_path}/Struct/$s/${s}_T1.nii.gz
 
 #Spatial Smoothing
@@ -27,26 +34,36 @@ imrm ${fs_path}/Struct/$s/${s}_T1_*
 
 #APPLY FREESURFER
 
+# Create a folder for the results of recon-all
 mkdir -p ${fs_path}/Recon
 
-
+# export the subjects_dir to the folder desired, make sure there are no subject folders already here (recon-all won't like that! 
 export SUBJECTS_DIR="${fs_path}/Recon"
 
+# Executes freesurfer ( as long as it hasn't been previously done)
  if [ ! -d $SUBJECTS_DIR/${s} ]; then
 
         recon-all -i ${fs_path}/Struct/$s/${s}_T1.nii.gz -s ${s} -all
-
 fi
-
 }
 
+# Exports the function 
 export -f free_surfing
+
+# Creates the subjects array, as the subjects are found in the RawData folder 
 s=($(ls $HOME/Brain_States/RawData))
+
+# Check the contents of the subjects array 
 echo ${s[@]}
+
+# Runs the function in parallel (essential as free surfer takes so long)
+# Setting the jobs to 0 -> allows parallel to divide the jobs across the CPUs in the most efficient way
 parallel --jobs 0 'free_surfing {1}' ::: ${s[@]}
 
 
+# Convert files from the freesurfer form to the fsl form 
 
+# Redefine the arrays (subjects and condition, as well as the paths
 subj=($(ls $HOME/Brain_States/RawData))
 cond=(as ns vs)
 data_path="$HOME/Brain_States"
