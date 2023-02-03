d932201b81c4e046fd07a6ec10c5a041158f43d8
naylor-kt <katie.naylor17@gmail.com>
2023-02-03 16:06:24 +0000

    Feb 3.2.23

--- Reworked_Code/5S_Surf_ALFF.sh
+++ Reworked_Code/5S_Surf_ALFF.sh
@@ -1,57 +1,17 @@
 #!/bin/bash
 
-# Step TWO of surface processing is to create the lta fields which will enable the projection of the individual functional timer series to the cortical surface
-
-#Set the path variables
-data_path="$HOME/BrainStates"
-preproc_path1="$HOME/BrainStates/Preproc/Level_1"
-preproc_path2="$HOME/BrainStates/Preproc/Level_2"
-preproc_path3="$HOME/BrainStates/Preproc/Level_3"
-vol_path="$HOME/BrainStates/Volumetric"
-surf_path="$HOME/BrainStates/Surface"
-fs_path="$HOME/BrainStates/Surface/Freesurfer"
-export SUBJECTS_DIR="${fs_path}/Recon"
-
-# Subjects Array
-subj=($(ls $HOME/BrainStates/RawData))
-
-#Condition Array
-cond=(as ns vs)
-
-for s in ${subj[@]}; do
-mkdir -p ${fs_path}/Registration/${s}
-
-    for c in ${cond[@]}; do
-#Make for loop for condition instead, just to be sure of preprocessing problem
-
-        # obtain registration from t1 (fsl) to orig (fs) & concatenate with mean2t1;
-
-        tkregister2 --mov ${fs_path}/Struct/$s/${s}_T1.nii.gz --targ $SUBJECTS_DIR/${s}/mri/orig.mgz --s ${s} \
-        --reg ${fs_path}/Registration/${s}/${s}_fsl2fs.dat --ltaout ${fs_path}/Registration/${s}/${s}_fsl2fs.lta --noedit --regheader
-
-        lta_convert --inlta ${fs_path}/Registration/${s}/${s}_fsl2fs.lta --outfsl ${fs_path}/Registration/${s}/${s}_fsl2fs.mat
-
-        convert_xfm -omat ${fs_path}/Registration/${s}/${s}-${c}_mean2fs.mat -concat ${fs_path}/Registration/${s}/${s}_fsl2fs.mat ${vol_path}/Registration/$s/${s}-${c}-meanfunc2struct.mat
-
-        lta_convert --infsl ${fs_path}/Registration/${s}/${s}-${c}_mean2fs.mat --outreg ${fs_path}/Registration/${s}/${s}-${c}_mean2fs.dat --outlta ${fs_path}/Registration/${s}/${s}-${c}_mean2fs.lta \
-        --subject ${s} --src ${preproc_path1}/$s/Level_1_Mean/${s}-${c}_mean_func.nii.gz --trg $SUBJECTS_DIR/$subj/mri/orig.mgz
-               
-   
-    done
-done
-
-
 # Following steps are completed using GNU parallel
-surf_processing_2 (){
+surf_ALFF (){
 
 #Set the path variables
-data_path="$HOME/BrainStates"; s=$1
-preproc_path1="$HOME/BrainStates/Preproc/Level_1"
-preproc_path2="$HOME/BrainStates/Preproc/Level_2"
-preproc_path3="$HOME/BrainStates/Preproc/Level_3"
-vol_path="$HOME/BrainStates/Volumetric"
-surf_path="$HOME/BrainStates/Surface"
-fs_path="$HOME/BrainStates/Surface/Freesurfer"
+data_path="$HOME/BrainStates_Test"; s=$1
+preproc_path1="$HOME/BrainStates_Test/Preproc/Level_1"
+preproc_path2="$HOME/BrainStates_Test/Preproc/Level_2"
+preproc_path3="$HOME/BrainStates_Test/Preproc/Level_3"
+analysis_path="$HOME/BrainStates_Test/Analysis"
+vol_path="$HOME/BrainStates_Test/Volumetric"
+surf_path="$HOME/BrainStates_Test/Surface"
+fs_path="$HOME/BrainStates_Test/Surface/Freesurfer"
 export SUBJECTS_DIR="${fs_path}/Recon"
 
 #Condition Array
@@ -60,124 +20,121 @@ cond=(as ns vs)
 #Hemisphere Array
 hemi=(lh rh)
 
-# Project the functional time series without smoothing to fsaverage space
-
-
-mkdir -p ${surf_path}/Projected/Non_Smoothed/Restricted_Temp_Filter/${s}/
+# Project ALFF fsaverage space
+mkdir -p ${surf_path}/Analysis/Non-smoothed/ALFF/${s}
 
     for c in ${cond[@]}; do
 
         for h in ${hemi[@]}; do
-        mri_vol2surf --mov ${preproc_path2}/Temporally_Filtered/Restricted/${s}/${s}-${c}-psc-Rtf.nii.gz \
+        mri_vol2surf --mov ${analysis_path}/ALFF/Un-smoothed/${s}/${s}-${c}-ALFF.nii.gz \
              --hemi ${h} \
-             --o ${surf_path}/Projected/Non_Smoothed/Restricted_Temp_Filter/${s}/${s}-${c}-${h}-Rtf-fsavg.mgz \
+             --o ${surf_path}/Analysis/Non-smoothed/ALFF/${s}/${s}-${c}-${h}-ALFF-fs.mgz \
              --projfrac-avg 0 1 0.1 \
              --reg ${fs_path}/Registration/$s/${s}-${c}_mean2fs.lta \
              --srcsubject ${s}
         done
      
         for h in ${hemi[@]}; do
-        mris_apply_reg --src ${surf_path}/Projected/Non_Smoothed/Restricted_Temp_Filter/${s}/${s}-${c}-${h}-Rtf-fsavg.mgz \
-                   --o ${surf_path}/Projected/Non_Smoothed/Restricted_Temp_Filter/${s}/${s}-${c}-${h}-Rtf-fsavg.mgz \
+        mris_apply_reg --src ${surf_path}/Analysis/Non-smoothed/ALFF/${s}/${s}-${c}-${h}-ALFF-fs.mgz \
+                   --o ${surf_path}/Analysis/Non-smoothed/ALFF/${s}/${s}-${c}-${h}-ALFF-fsavg.mgz \
                    --streg $SUBJECTS_DIR/${s}/surf/${h}.fsaverage.sphere.reg $SUBJECTS_DIR/fsaverage/surf/${h}.sphere.reg
         done
         
     done
 
-
-mkdir -p ${surf_path}/Projected/Non_Smoothed/Wide_Temp_Filter/${s}/
+# Project fALFF fsaverage space
+mkdir -p ${surf_path}/Analysis/Non-smoothed/fALFF/${s}
 
     for c in ${cond[@]}; do
 
         for h in ${hemi[@]}; do
-        mri_vol2surf --mov ${preproc_path2}/Temporally_Filtered/Wide/${s}/${s}-${c}-psc-Wtf.nii.gz \
+        mri_vol2surf --mov ${analysis_path}/fALFF/Un-smoothed/${s}/${s}-${c}-fALFF.nii.gz \
              --hemi ${h} \
-             --o ${surf_path}/Projected/Non_Smoothed/Wide_Temp_Filter/${s}/${s}-${c}-${h}-Wtf-fsavg.mgz \
+             --o ${surf_path}/Analysis/Non-smoothed/fALFF/${s}/${s}-${c}-${h}-fALFF-fs.mgz \
              --projfrac-avg 0 1 0.1 \
              --reg ${fs_path}/Registration/$s/${s}-${c}_mean2fs.lta \
              --srcsubject ${s}
         done
      
         for h in ${hemi[@]}; do
-        mris_apply_reg --src ${surf_path}/Projected/Non_Smoothed/Wide_Temp_Filter/${s}/${s}-${c}-${h}-Wtf-fsavg.mgz \
-                   --o ${surf_path}/Projected/Non_Smoothed/Wide_Temp_Filter/${s}/${s}-${c}-${h}-Wtf-fsavg.mgz \
+        mris_apply_reg --src ${surf_path}/Analysis/Non-smoothed/fALFF/${s}/${s}-${c}-${h}-fALFF-fs.mgz \
+                   --o ${surf_path}/Analysis/Non-smoothed/fALFF/${s}/${s}-${c}-${h}-fALFF-fsavg.mgz \
                    --streg $SUBJECTS_DIR/${s}/surf/${h}.fsaverage.sphere.reg $SUBJECTS_DIR/fsaverage/surf/${h}.sphere.reg
         done
         
     done
 
-# Additionally project the functional time series to fsaverage space, and to smoothe the image during this process
 
-# Restricted functional time series
-mkdir -p ${surf_path}/Projected/Smoothed/Restricted_Temp_Filter/${s}/
+# Additionally project the ALFF/fALFF to fsaverage space, and to smoothe the image during this process
+
+# Project ALFF fsaverage space
+mkdir -p ${surf_path}/Analysis/Smoothed/ALFF/${s}
 
     for c in ${cond[@]}; do
 
         for h in ${hemi[@]}; do
-        mri_vol2surf --mov ${preproc_path2}/Temporally_Filtered/Restricted/${s}/${s}-${c}-psc-Rtf.nii.gz \
+        mri_vol2surf --mov ${analysis_path}/ALFF/Un-smoothed/${s}/${s}-${c}-ALFF.nii.gz \
              --hemi ${h} \
-             --o ${surf_path}/Projected/Smoothed/Restricted_Temp_Filter/${s}/${s}-${c}-${h}-RtfSM-fsavg.mgz \
+             --o ${surf_path}/Analysis/Smoothed/ALFF/${s}/${s}-${c}-${h}-ALFF-SM-fs.mgz \
              --projfrac-avg 0 1 0.1 \
              --reg ${fs_path}/Registration/$s/${s}-${c}_mean2fs.lta \
              --srcsubject ${s}
         done
-# Smoothe the vol2surf projected image
-  fwhm=5
+
+    # Smoothe the vol2surf projected image
+      fwhm=5
         for h in ${hemi[@]}; do
-        mri_surf2surf --sval ${surf_path}/Projected/Smoothed/Restricted_Temp_Filter/${s}/${s}-${c}-${h}-RtfSM-fsavg.mgz \
-        --tval ${surf_path}/Projected/Smoothed/Restricted_Temp_Filter/${s}/${s}-${c}-${h}-RtfSM-fsavg.mgz \
+        mri_surf2surf --sval ${surf_path}/Analysis/Smoothed/ALFF/${s}/${s}-${c}-${h}-ALFF-SM-fs.mgz \
+        --tval ${surf_path}/Analysis/Smoothed/ALFF/${s}/${s}-${c}-${h}-ALFF-SM-fs.mgz \
         --s ${s} \
         --hemi ${h} \
         --fwhm $fwhm \
         --cortex
         
         done
-     
         for h in ${hemi[@]}; do
-        mris_apply_reg --src ${surf_path}/Projected/Smoothed/Restricted_Temp_Filter/${s}/${s}-${c}-${h}-RtfSM-fsavg.mgz \
-                   --o ${surf_path}/Projected/Smoothed/Restricted_Temp_Filter/${s}/${s}-${c}-${h}-RtfSM-fsavg.mgz \
+        mris_apply_reg --src ${surf_path}/Analysis/Smoothed/ALFF/${s}/${s}-${c}-${h}-ALFF-SM-fs.mgz \
+                   --o ${surf_path}/Analysis/Smoothed/ALFF/${s}/${s}-${c}-${h}-ALFF-SM-fsavg.mgz \
                    --streg $SUBJECTS_DIR/${s}/surf/${h}.fsaverage.sphere.reg $SUBJECTS_DIR/fsaverage/surf/${h}.sphere.reg
         done
         
     done
 
-
-# Wide Fitered Functional Time Series 
-mkdir -p ${surf_path}/Projected/Smoothed/Wide_Temp_Filter/${s}/
+# Project fALFF fsaverage space
+mkdir -p ${surf_path}/Analysis/Smoothed/fALFF/${s}
 
     for c in ${cond[@]}; do
 
         for h in ${hemi[@]}; do
-        mri_vol2surf --mov ${preproc_path2}/Temporally_Filtered/Wide/${s}/${s}-${c}-psc-Wtf.nii.gz \
+        mri_vol2surf --mov ${analysis_path}/fALFF/Un-smoothed/${s}/${s}-${c}-fALFF.nii.gz \
              --hemi ${h} \
-             --o ${surf_path}/Projected/Smoothed/Wide_Temp_Filter/${s}/${s}-${c}-${h}-WtfSM-fsavg.mgz \
+             --o ${surf_path}/Analysis/Smoothed/fALFF/${s}/${s}-${c}-${h}-fALFF-SM-fs.mgz \
              --projfrac-avg 0 1 0.1 \
              --reg ${fs_path}/Registration/$s/${s}-${c}_mean2fs.lta \
              --srcsubject ${s}
         done
-
-# Smoothe the vol2surf projected image
-  fwhm=5
+     
+    # Smoothe the vol2surf projected image
+      fwhm=5
         for h in ${hemi[@]}; do
-        mri_surf2surf --sval ${surf_path}/Projected/Smoothed/Wide_Temp_Filter/${s}/${s}-${c}-${h}-WtfSM-fsavg.mgz \
-        --tval ${surf_path}/Projected/Smoothed/Wide_Temp_Filter/${s}/${s}-${c}-${h}-WtfSM-fsavg.mgz \
+        mri_surf2surf --sval ${surf_path}/Analysis/Smoothed/fALFF/${s}/${s}-${c}-${h}-fALFF-SM-fs.mgz \
+        --tval ${surf_path}/Analysis/Smoothed/fALFF/${s}/${s}-${c}-${h}-fALFF-SM-fs.mgz \
         --s ${s} \
         --hemi ${h} \
         --fwhm $fwhm \
         --cortex
-        
         done
      
         for h in ${hemi[@]}; do
-        mris_apply_reg --src ${surf_path}/Projected/Smoothed/Wide_Temp_Filter/${s}/${s}-${c}-${h}-WtfSM-fsavg.mgz \
-                   --o ${surf_path}/Projected/Smoothed/Wide_Temp_Filter/${s}/${s}-${c}-${h}-WtfSM-fsavg.mgz \
+        mris_apply_reg --src ${surf_path}/Analysis/Smoothed/fALFF/${s}/${s}-${c}-${h}-fALFF-SM-fs.mgz \
+                   --o ${surf_path}/Analysis/Smoothed/fALFF/${s}/${s}-${c}-${h}-fALFF-SM-fsavg.mgz \
                    --streg $SUBJECTS_DIR/${s}/surf/${h}.fsaverage.sphere.reg $SUBJECTS_DIR/fsaverage/surf/${h}.sphere.reg
         done
         
     done
 }
 
-export -f surf_processing_2
+export -f surf_ALFF
 
 # Create an array with subjects (as they are in the RawData file
 s=($(ls $HOME/BrainStates/RawData))
@@ -189,4 +146,4 @@ echo ${s[@]}
 # Jobs is set to 0, which allows parallel to assign the jobs as it sees fit, and divide it across the CPUs itself
 # Provide it with the name of the function, and specify it will take one argument, then provide this after the three colons
 
-parallel --jobs 0 'surf_processing_2 {1}' ::: ${s[@]}
+parallel --jobs 0 'surf_ALFF {1}' ::: ${s[@]}
---
+++ Reworked_Code/Run_Scripts.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Preprocessing
+chmod +x /Users/mszkcn/Brain_States_Code/Brain-States/Reworked_Code/1_Preprocessing.sh
+sh /Users/mszkcn/Brain_States_Code/Brain-States/Reworked_Code/1_Preprocessing.sh
+
+# Analysis
+chmod +x /Users/mszkcn/Brain_States_Code/Brain-States/Reworked_Code/2_Analysis.sh
+sh /Users/mszkcn/Brain_States_Code/Brain-States/Reworked_Code/2_Analysis.sh
+
+# Volume Processing
+chmod +x /Users/mszkcn/Brain_States_Code/Brain-States/Reworked_Code/3V_Volume_Processing.sh
+sh /Users/mszkcn/Brain_States_Code/Brain-States/Reworked_Code/3V_Volume_Processing.sh
+
+# Surface Processing
+chmod +x /Users/mszkcn/Brain_States_Code/Brain-States/Reworked_Code/4S_Surface_Processing.sh
+sh /Users/mszkcn/Brain_States_Code/Brain-States/Reworked_Code/4S_Surface_Processing.sh
